a34a72cb3db8eab64940e35e842834972fcd0bfa
Amir Kasumovic <amir@ideaology.io>
2021-07-22 14:22:47 +0200

    projects background

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -23,13 +23,20 @@ export const Projects: React.FC = () =>{
         <div className='bg-color' >
             <div className='projectsWrapper' data-aos='zoom-in' data-aos-duration="500" data-aos-easing="linear">
                 <div className="projects">
-                    <InteractiveCard img={''} projectName={''} projectDesc={''} projectGithub={''} projectCodepen={''}/>
-                    <InteractiveCard img={''} projectName={''} projectDesc={''} projectGithub={''} projectCodepen={''}/>
-                    <InteractiveCard img={''} projectName={''} projectDesc={''} projectGithub={''} projectCodepen={''}/>
-                    <InteractiveCard img={''} projectName={''} projectDesc={''} projectGithub={''} projectCodepen={''}/>
-                    <InteractiveCard img={''} projectName={''} projectDesc={''} projectGithub={''} projectCodepen={''}/>
-                    <InteractiveCard img={''} projectName={''} projectDesc={''} projectGithub={''} projectCodepen={''}/>
-                    <InteractiveCard img={''} projectName={''} projectDesc={''} projectGithub={''} projectCodepen={''}/>
+                    <div className="projectsTop">
+                        <h1 className="projectsTitle">Projects</h1>
+                    </div>
+                    <div className="projectsBottom">
+                        <InteractiveCard img={''} projectName={'Todo list'} projectDesc={''} projectGithub={''} projectCodepen={''}/>
+                        <InteractiveCard img={''} projectName={'Quizzie'} projectDesc={''} projectGithub={''} projectCodepen={''}/>
+                        <InteractiveCard img={''} projectName={'Stranger'} projectDesc={''} projectGithub={''} projectCodepen={''}/>
+                        <InteractiveCard img={''} projectName={'eCommerce'} projectDesc={''} projectGithub={''} projectCodepen={''}/>
+                        {/* <InteractiveCard img={''} projectName={'VueJS Practice site'} projectDesc={''} projectGithub={''} projectCodepen={''}/> */}
+                        <InteractiveCard img={''} projectName={'IdeaVerse'} projectDesc={''} projectGithub={''} projectCodepen={''}/>
+                        <InteractiveCard img={''} projectName={'IdeaMaze'} projectDesc={''} projectGithub={''} projectCodepen={''}/>
+                        <InteractiveCard img={''} projectName={'IdeaDifferences'} projectDesc={''} projectGithub={''} projectCodepen={''}/>
+                        <InteractiveCard img={''} projectName={'Idea SpaceDefenter'} projectDesc={''} projectGithub={''} projectCodepen={''}/>
+                    </div>
                 </div>
             </div>
         </div>
