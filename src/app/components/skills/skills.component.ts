2cbca896c3b32b56d82a09ba965f2b7ae00ece2e
Amir Kasumovic <amir.kasumovic@live.com>
2023-09-08 23:41:24 +0200

    horizontal scroll line added

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+  ngOnInit() {
+    const slider: any = document.querySelector('.skills-section-content');
+    const sliderLine: any = document.querySelector(".horizontal-line");
+    sliderLine.style.width =  slider.scrollWidth + 'px';
+    let isDown = false;
+    let startX: number;
+    let scrollLeft: number;
+
+    slider.addEventListener('mousedown', (e: any) => {
+      isDown = true;
+      slider.classList.add('active');
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mousemove', (e: any) => {
+      if(!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 3; //scroll-fast
+      slider.scrollLeft = scrollLeft - walk;
+    });
+  }
 }
