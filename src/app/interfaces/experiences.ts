1d19b31c45f540bbc20ac7bcec9b0f9ce311111c
Amir Kasumovic <amir.kasumovic@live.com>
2023-09-16 22:55:26 +0200

    translations done

--- src/app/interfaces/experiences.ts
+++ src/app/interfaces/experiences.ts
@@ -1,26 +1,6 @@
+import { Experience } from "./experience"
+
 export interface Experiences {
-  en:{
-    title: string
-    company: string
-    from: string
-    to: string
-    details: {
-      description: string
-      location: string
-      responsibilities: string[]
-      link: string
-    }
-  },
-  ba: {
-    title: string
-    company: string
-    from: string
-    to: string
-    details: {
-      description: string
-      location: string
-      responsibilities: string[]
-      link: string
-    }
-  }
+  en: Experience[]
+  ba: Experience[]
 }
